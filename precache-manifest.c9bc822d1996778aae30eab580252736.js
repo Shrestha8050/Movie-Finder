93c90da9302a82134d18cfbc67298cf2898934e3
Bodhan Shrestha <bodhanshrestha@gmail.com>
2020-07-16 13:22:07 +0545

    Updates

---
+++ precache-manifest.c9bc822d1996778aae30eab580252736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd780c969ea83bbb1b8d5b440598e04a",
+    "url": "/Movie-Finder/index.html"
+  },
+  {
+    "revision": "87c9ea28cc0934181819",
+    "url": "/Movie-Finder/static/css/main.d283f774.chunk.css"
+  },
+  {
+    "revision": "27e6367c9a1e1b715fdc",
+    "url": "/Movie-Finder/static/js/2.3d35c453.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Movie-Finder/static/js/2.3d35c453.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87c9ea28cc0934181819",
+    "url": "/Movie-Finder/static/js/main.2d74d1fb.chunk.js"
+  },
+  {
+    "revision": "54f8df8c98291a7d52f8",
+    "url": "/Movie-Finder/static/js/runtime-main.55ee1f28.js"
+  }
+]);
